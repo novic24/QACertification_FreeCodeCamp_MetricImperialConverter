77921f8b29b3f2bfe80b53528eba883c53faab8f
Adrià Batlle <82225234+novic24@users.noreply.github.com>
2021-07-23 15:50:49 +0200

    new

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,66 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
+  this.getNum = function (input) {
     let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getUnit = function (input) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    let result;
+    if (initUnit === "gal" || initUnit === "GAL") {
+      //we convert to L
+      result = "l";
+    } else if (initUnit === "L" || initUnit === "l") {
+      result = "gal";
+    }
+
+    return result;
+  };
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
+    if (initUnit === "gal" || initUnit === "GAL" || initUnit === "Gal") {
+      result = (initNum * galToL).toFixed(5);
+    } else if (initUnit === "L" || initUnit === "l") {
+      result = (initNum / galToL).toFixed(5);
+    }
+
+    if (initUnit === "lbs" || initUnit === "LBS" || initUnit === "Lbs") {
+      result = (initNum * lbsToKg).toFixed(5);
+    } else if (initUnit === "Kg" || initUnit === "kg" || initUnit === "KG") {
+      result = (initNum / lbsToKg).toFixed(5);
+    }
+
+    if (initUnit === "mi" || initUnit === "MI" || initUnit === "Mi") {
+      result = (initNum * miToKm).toFixed(5);
+    } else if (initUnit === "KM" || initUnit === "km" || initUnit === "Km") {
+      result = (initNum / miToKm).toFixed(5);
+    }
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,22 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.route("/").get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let toString = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+  });
 };
