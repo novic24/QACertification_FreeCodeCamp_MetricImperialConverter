b504c93e974ab21b134c8af090a794a35cbc2890
Adrià Batlle <82225234+novic24@users.noreply.github.com>
2021-08-09 16:35:40 +0200

    api changes

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,42 @@
-"use strict";
-
 const expect = require("chai").expect;
 const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  let convertHandler = new ConvertHandler();
+  const convertHandler = new ConvertHandler();
 
-  app.route("/").get((req, res) => {
-    let input = req.query.input;
-    let initNum = convertHandler.getNum(input);
-    let initUnit = convertHandler.getUnit(input);
-    let returnNum = convertHandler.convert(initNum, initUnit);
-    let returnUnit = convertHandler.getReturnUnit(initUnit);
-    let toString = convertHandler.getString(
+  app.route("/api/convert").get((req, res) => {
+    const input = req.query.input;
+
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    if (initNum === "invalid number" && initUnit === "invalid unit")
+      return res.json({ error: "invalid number and unit" });
+
+    if (initNum === "invalid number") return res.json({ error: initNum });
+    if (initUnit === "invalid unit") return res.json({ error: initUnit });
+
+    const returnNum = convertHandler.convert(initNum, initUnit);
+
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+
+    const toString = convertHandler.getString(
       initNum,
       initUnit,
       returnNum,
       returnUnit
     );
+    // console.log(initNum);
+    // console.log(initUnit);
+    // console.log(returnNum);
+    // console.log(returnUnit);
+    // console.log(toString);
+
+    res.json({
+      initNum: initNum,
+      initUnit: initUnit,
+      returnNum: returnNum,
+      returnUnit: returnUnit,
+      string: toString
+    });
   });
 };
